adba56b8cc4a0ffa99c821f86cfbc5a6ecc167b6
Justin Hurstwright <jryanhurst@gmail.com>
2021-08-12 14:42:53 -0500

    feat: Create Edge trails

--- Prefabs/Ship/PlayerShip.gd
+++ Prefabs/Ship/PlayerShip.gd
@@ -9,6 +9,7 @@ onready var thruster_particles = $Thrusters
 
 export var thruster_force := 1.0
 export var rotation_factor := 1.0
+export var max_speed := 750
 
 var _accelerating := false
 var _turn := 0.0
@@ -31,6 +32,8 @@ func _handle_velocity(_delta: float) -> void:
 	if _accelerating:
 		var direction = Vector2.UP.rotated(rotation)
 		linear_velocity += direction
+	if linear_velocity.length_squared() > max_speed*max_speed:
+		linear_velocity = linear_velocity.clamped(max_speed)
 	
 func _gather_input() -> void:
 	if Input.is_action_just_pressed("accelerate"):
