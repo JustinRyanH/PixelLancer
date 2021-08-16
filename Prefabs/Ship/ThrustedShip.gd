028251f9d5efd5e7cb2bb829c536a185932d67ac
Justin Hurstwright <jryanhurst@gmail.com>
2021-08-16 09:52:52 -0500

    featL Simple rotation

--- Prefabs/Ship/ThrustedShip.gd
+++ Prefabs/Ship/ThrustedShip.gd
@@ -1,4 +1,36 @@
-extends Node2D
+extends RigidBody2D
 
-func _process(_delta: float) -> void:
-	look_at(get_global_mouse_position())
+## TODO: Remove
+export var movement_speed_modifier := 1
+var _movement: Vector2 = Vector2.ZERO
+
+onready var look_crosshair: Node2D = $LookCrossHair
+
+func _process(delta: float) -> void:
+	var _mouse_pos = get_local_mouse_position().normalized()
+	look_crosshair.position = _mouse_pos * 150.0
+	look_crosshair.rotation_degrees = rad2deg(_mouse_pos.angle()) + 90.0
+	
+	rotate_towards_mouse(delta)
+	debug_move(delta)
+	
+func rotate_towards_mouse(delta: float) -> void:
+	var _mouse_rotation := get_local_mouse_position().angle()
+	if abs(_mouse_rotation) < 0.5 && abs(_mouse_rotation) > 0.01:
+		_mouse_rotation = sign(_mouse_rotation)
+	elif abs(_mouse_rotation) <= 0.01:
+		_mouse_rotation = 0
+	print(_mouse_rotation)
+	rotate(_mouse_rotation * delta)
+
+func debug_move(delta: float) -> void:
+	_movement = Vector2.ZERO
+	if Input.is_action_pressed("ui_down"):
+		_movement.y += 1
+	if Input.is_action_pressed("ui_up"):
+		_movement.y -= 1
+	if Input.is_action_pressed("ui_right"):
+		_movement.x += 1
+	if Input.is_action_pressed("ui_left"):
+		_movement.x -= 1
+	position += _movement * delta * movement_speed_modifier
