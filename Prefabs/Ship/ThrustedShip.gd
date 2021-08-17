847f14aa7e6cfc81393e7469c16d5d55f4bd6e80
Justin Hurstwright <jryanhurst@gmail.com>
2021-08-16 20:40:47 -0500

    feat: Show Speed on Canvas

--- Prefabs/Ship/ThrustedShip.gd
+++ Prefabs/Ship/ThrustedShip.gd
@@ -2,37 +2,49 @@ extends RigidBody2D
 
 const PI_DIV_8 = PI / 8
 
-## TODO: Remove
-export var movement_speed_modifier := 1
+export var thruster_power := 100.0
 export var rotation_speed = 1.0
 export var stop_buffer := 0.01
+
+var _thrust: Vector2 = Vector2.ZERO
 var _movement: Vector2 = Vector2.ZERO
 
 onready var look_crosshair: Node2D = $LookCrossHair
 
+func _ready():
+	yield(get_tree().create_timer(1.0), "timeout")
+	Events.emit_signal("connect_ship", self)
+
 func _process(delta: float) -> void:
 	rotate_towards_mouse(delta)
 	update_crosshairs()
-	debug_move(delta)
+	move_input()
+
+func _physics_process(delta: float) -> void:
+	apply_central_impulse(_thrust * thruster_power *  delta)
 
 func rotate_towards_mouse(delta: float) -> void:
 	var _mouse_rotation := get_local_mouse_position().angle()
 	if abs(_mouse_rotation) > 1.0:
-		rotate(_mouse_rotation * rotation_speed * delta)
+		angular_velocity = _mouse_rotation * rotation_speed
 	elif abs(_mouse_rotation) > stop_buffer:
-		rotate(sign(_mouse_rotation) * rotation_speed * delta)
+		angular_velocity = sign(_mouse_rotation) * rotation_speed
+	else:
+		angular_velocity = 0.0
 
-func debug_move(delta: float) -> void:
-	_movement = Vector2.ZERO
-	if Input.is_action_pressed("ui_down"):
-		_movement.y += 1
-	if Input.is_action_pressed("ui_up"):
-		_movement.y -= 1
-	if Input.is_action_pressed("ui_right"):
-		_movement.x += 1
-	if Input.is_action_pressed("ui_left"):
-		_movement.x -= 1
-	position += _movement * delta * movement_speed_modifier
+func move_input() -> void:
+	_thrust = Vector2.ZERO
+	if Input.is_action_pressed("thrust_left"):
+		_thrust.x -= 1
+	if Input.is_action_pressed("thrust_right"):
+		_thrust.x += 1
+	if Input.is_action_pressed("thrust_up"):
+		_thrust.y -= 1
+	if Input.is_action_pressed("thrust_down"):
+		_thrust.y += 1
+	# Normalize the Thrust so that we don't get the 
+	# the quake strife bug 
+	_thrust.normalized()
 
 func update_crosshairs() -> void:
 	var _mouse_pos = get_local_mouse_position().normalized()
---
+++ Scripts/DebugAI.gd
@@ -0,0 +1,17 @@
+extends Control
+
+signal register_main_ship
+
+
+onready var speed_value_label: Label = $SpeedValue
+var _main_ship: RigidBody2D = null
+
+func _ready():
+	Events.connect("connect_ship", self, "_connect_ship")
+
+func _connect_ship(ship: RigidBody2D) -> void:
+	_main_ship = ship
+	
+func _process(_delta:  float) -> void:
+	if _main_ship:
+		speed_value_label.text = "%2.2f" % _main_ship.linear_velocity.length() 
