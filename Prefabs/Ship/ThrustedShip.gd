4ca90e9b0407ff02ef1509df76de3398feb7862a
Justin Hurstwright <jryanhurst@gmail.com>
2021-08-20 09:23:25 -0500

    refactor: ThrustedShip Defers to engine

--- Prefabs/Ship/ThrustedShip.gd
+++ Prefabs/Ship/ThrustedShip.gd
@@ -1,6 +1,16 @@
 extends RigidBody2D
 
+export var thruster_power := 100.0 setget set_thruster_power
+export var max_speed := 900.0 setget set_max_speed
+
+
+export var stop_buffer := 0.2 setget set_stop_buffer
+export var rotation_speed := 2.0 setget set_rotation_speed
+
+
 onready var look_crosshair: Node2D = $LookCrossHair
+onready var rotator := $ShipRotator
+onready var engine := $EngineController
 
 func _ready():
 	yield(get_tree().create_timer(1.0), "timeout")
@@ -13,3 +23,20 @@ func update_crosshairs() -> void:
 	var _mouse_pos = get_local_mouse_position().normalized()
 	look_crosshair.position = _mouse_pos * 150.0
 	look_crosshair.rotation_degrees = rad2deg(_mouse_pos.angle()) + 90.0
+
+func set_thruster_power(value: float) -> void:
+	thruster_power = value
+	engine.thruster_power = value
+
+func set_max_speed(value: float) -> void:
+	max_speed = value
+	engine.max_speed = value
+	
+
+func set_stop_buffer(value: float) -> void:
+	stop_buffer = value
+	rotator.set_stop_buffer = value
+
+func set_rotation_speed(value: float) -> void:
+	rotation_speed = value
+	rotator.set_stop_buffer = value
