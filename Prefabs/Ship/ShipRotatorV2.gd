8b0affb51baddcc0350ef602e80da62761b9ac20
Justin Hurstwright <jryanhurst@gmail.com>
2021-08-23 18:57:25 -0500

    feat: Create a more turned version of movement

--- Prefabs/Ship/ShipRotatorV2.gd
+++ Prefabs/Ship/ShipRotatorV2.gd
@@ -1,32 +1,27 @@
 extends Node2D
 class_name ShipRotatorV2
 
-enum RotationState { NO_ADJUSTMENT, MINOR_AJUSTMENT, MAJOR_ADJUSTMENT }
+const OVERSHOOT_ZONE: float = PI / 2
+
 
 const ANGULAR_VELOCITY_DEAD_ZONE := 0.1
 export var dead_zone := 0.2
 export var adjustment_zone := 1.0
+export var rotation_multiplayer := 1.0
+export var max_angular_velocity := 2.0
 
 onready var parent: RigidBody2D = get_parent()
 onready var rotate_port: RotationThruster = parent.get_node("RotationPort")
 onready var rotate_starboard: RotationThruster = parent.get_node("RotationStarboard")
 
+
 var _target_angle: float = 0.0
 var _target_angle_sign: int = 0
 var _thrust_force: float = 0.0
 var _adjusted_dead_zone: float = dead_zone
-var _rotation_state = RotationState.NO_ADJUSTMENT
 
-func set_rotation_state(new_state: int) -> void:
-	if _rotation_state == RotationState.NO_ADJUSTMENT and new_state == RotationState.MINOR_AJUSTMENT:
-		_rotation_state = new_state
-	elif new_state == RotationState.NO_ADJUSTMENT:
-		_rotation_state = new_state
-	else:
-		_rotation_state = new_state
 
-func _process(_delta: float) -> void:
-	_adjusted_dead_zone = _adjust_dead_zone()
+func emit_thrust() ->  void:
 	if _thrust_force > 0.0:
 		rotate_port.is_emitting = true
 		rotate_starboard.is_emitting = false
@@ -37,27 +32,45 @@ func _process(_delta: float) -> void:
 		rotate_port.is_emitting = false
 		rotate_starboard.is_emitting = false
 
+func _process(_delta: float) -> void:
+	_adjusted_dead_zone = _adjust_dead_zone()
+	emit_thrust()
+
 	set_target_angle(get_local_mouse_position().angle())
 
-	if abs(_target_angle) > _adjusted_dead_zone:
-		set_rotation_state(RotationState.MINOR_AJUSTMENT)
+	if abs(_target_angle) > _speed_adj_zone():
+		_thrust_force = _target_angle_sign * rotation_multiplayer
+	elif abs(_target_angle) > _adjusted_dead_zone:
+		if  _target_angle_sign != sign(parent.angular_velocity):
+			print("Overshoot")
+		if sign(parent.angular_velocity) == _target_angle_sign:
+			_thrust_force = -_target_angle_sign * 0.5 * rotation_multiplayer
+		else:
+			_thrust_force = _target_angle_sign * rotation_multiplayer
 	else:
-		parent.angular_velocity = lerp(parent.angular_velocity, 0.0, 0.9)
-		set_rotation_state(RotationState.NO_ADJUSTMENT)
+		if abs(parent.angular_velocity) < 0.5 and abs(parent.angular_velocity) > 0.0:
+			parent.angular_velocity = lerp(parent.angular_velocity, 0.0, 0.5)
+		_thrust_force = 0
 
 	_update_debug()
 
-func _physics_process(_delta: float):
-	if _rotation_state == RotationState.MINOR_AJUSTMENT:
-		_thrust_force = _target_angle_sign
-	else:
-		_thrust_force = 0.0
+func _physics_process(delta: float):
+	parent.angular_velocity = clamp(
+		parent.angular_velocity + _thrust_force * delta,
+		-max_angular_velocity,
+		max_angular_velocity
+	)
 
 func _update_debug() -> void:
 	var debug_circle: DebugAngleCircle = $DebugCircle
 	debug_circle.dead_zone = _adjusted_dead_zone
-	debug_circle.adjustment_zone = adjustment_zone
+	debug_circle.adjustment_zone = _speed_adj_zone()
 	debug_circle.target_angle = _target_angle
+	debug_circle.overshoot_zone = OVERSHOOT_ZONE
+
+func _speed_adj_zone() -> float:
+	var zone = adjustment_zone * (abs(parent.angular_velocity) / max_angular_velocity)
+	return max(zone, _adjusted_dead_zone)
 
 func set_target_angle(new_angle: float) -> void:
 	_target_angle = new_angle
