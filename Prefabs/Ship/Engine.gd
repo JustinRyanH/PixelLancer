f61a1127d530d3874877e08f17a929c08bc8e505
Justin Hurstwright <jryanhurst@gmail.com>
2021-08-27 08:51:07 -0500

    refactor: Simplify the code

--- Prefabs/Ship/Engine.gd
+++ Prefabs/Ship/Engine.gd
@@ -15,6 +15,9 @@ var _rotated_thrust := Vector2.ZERO
 
 onready var thrusters: Thrusters = $Thrusters
 
+func _process(_delta: float) -> void:
+	_emit_thrusters(_rotated_thrust)
+
 func _physics_process(delta: float) -> void:
 	if parent.linear_velocity.length_squared() > max_speed * max_speed:
 		parent.linear_velocity = parent.linear_velocity.clamped(max_speed)
@@ -31,36 +34,15 @@ func _physics_process(delta: float) -> void:
 		parent.fuel -= delta
 		reduce_fuel(delta * direction_multiplayer)
 		parent.apply_central_impulse(_rotated_thrust * thruster_power * direction_multiplayer * delta)
-	if parent.fuel > 0:
-		_emit_thrusters(_rotated_thrust)
-	else:
-		_emit_thrusters(Vector2.ZERO)
-
-func move_input() -> void:
-	var _thrust = Vector2.ZERO
-	if Input.is_action_pressed("thrust_up"):
-		_thrust.x += 1
-	if Input.is_action_pressed("thrust_down"):
-		_thrust.x -= 1
-	if Input.is_action_pressed("thrust_left"):
-		_thrust.y -= 1
-	if Input.is_action_pressed("thrust_right"):
-		_thrust.y += 1
-	boost = Input.is_action_pressed("boost") and parent.fuel > 0
-	# Normalize the Thrust so that we don't get the
-	# the quake strife bug
-	_thrust = _thrust.normalized()
-
-	thrust = _thrust.rotated(parent.rotation)
 
 ## Set Thrust Direction for Thruster Emission
 # Thrust direction is relative to the screen, but when we emit we need to
 # understand the rotation of the ship to decide which ones to turn on.
 # First we rotate the _thrust vector in  the opposite rotation  of the ship rotation
 # Then we reverse the thrust so that the thruster direction matches with the thruster
-func _emit_thrusters(thrust: Vector2) -> void:
-	thrusters.emission_vector = -thrust
-	thrusters.boost = boost
+func _emit_thrusters(p_thrust: Vector2) -> void:
+	thrusters.emission_vector = -p_thrust if parent.fuel > 0 else Vector2.ZERO
+	thrusters.boost = boost if parent.fuel > 0 else false
 
 func reduce_fuel(amount: float) -> void:
 	parent.fuel = max(parent.fuel - amount, 0.0)
