fcf4a123faa0758fe8ff9d85a2bf5adebc18f56e
Justin Ryan Hurst <jryanhurst@gmail.com>
2021-09-01 17:17:08 -0500

    refactor: Preload control ui

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -2,6 +2,7 @@ extends Control
 
 onready var start_button := $VBoxContainer/Start
 
+const CONTROLS_UI = preload("res://UI/ControlsUI.tscn")
 
 func _ready():
 	start_button.grab_focus()
@@ -13,5 +14,5 @@ func _on_Quit_pressed():
 	get_tree().quit()
 
 func _on_Controls_pressed():
-	var options = load("res://UI/ControlsUI.tscn").instance()
+	var options = CONTROLS_UI.instance()
 	get_tree().root.add_child(options)
