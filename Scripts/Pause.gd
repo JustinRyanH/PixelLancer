4742498cac0627ea1db898cf0c11f522b5d9fd95
Justin Ryan Hurst <jryanhurst@gmail.com>
2021-09-01 14:26:23 -0500

    refactor: Simplify the pause

--- Scripts/Pause.gd
+++ Scripts/Pause.gd
@@ -2,22 +2,21 @@ extends Control
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("game_pause"):
-		_pause_game()
+		_unpause_game() if get_tree().paused else _pause_game()
 
 func _pause_game() -> void:
 	var tree = get_tree()
-	var current_state = tree.paused
-	
-	if current_state == true:
-		visible = false
-	else:
-		visible = true
-		
-	tree.paused = !current_state
+	visible = true
+	tree.paused = true
 
+func _unpause_game() -> void:
+	var tree = get_tree()
+	visible = false
+	tree.paused = false
 
 func _on_Return_pressed():
-	_pause_game()
+	_unpause_game()
 
 func _on_Exit_pressed():
+	_unpause_game()
 	get_tree().change_scene("res://Scenes/MainMenu.tscn")
