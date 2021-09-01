25ff3754ccb68600eaa966be38cd33b514acdc5f
Justin Ryan Hurst <jryanhurst@gmail.com>
2021-09-01 11:11:36 -0500

    feat: Pressing escape pauses the game

---
+++ Scripts/Pause.gd
@@ -0,0 +1,17 @@
+extends Control
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("game_pause"):
+		_pause_game()
+
+
+func _pause_game() -> void:
+	var tree = get_tree()
+	var current_state = tree.paused
+	
+	if current_state == true:
+		$ColorRect.visible = false
+	else:
+		$ColorRect.visible = true
+		
+	tree.paused = !current_state
