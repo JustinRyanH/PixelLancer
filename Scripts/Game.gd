c4d1777b52bc45b36ca5b4124cbac525e824172c
Justin Ryan Hurst <jryanhurst@gmail.com>
2021-09-02 08:59:24 -0500

    refactor: Extract method

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -2,17 +2,25 @@ extends Node
 
 
 func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	_hide_mouse()
 	Events.connect("game_paused", self, "_game_paused")
 	Events.connect("game_unpaused", self, "_game_unpaused")
 
 
 func _game_paused() -> void:
-	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	var tree = get_tree()
 	tree.paused = true
+	_unhide_mouse()
 	
 func _game_unpaused() -> void:
-	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	var tree = get_tree()
 	tree.paused = false
+	_hide_mouse()
+
+
+func _hide_mouse() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	
+func _unhide_mouse() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	
