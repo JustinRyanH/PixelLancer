5d125dd434f1cbbc2973b0c9b8a6bd04ea5d87a7
Justin Ryan Hurst <jryanhurst@gmail.com>
2021-09-02 10:15:04 -0500

    feat: I'm going to have a aim target

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -2,7 +2,7 @@ extends Node
 
 
 func _ready():
-	_hide_mouse()
+	_unpaused_mouse()
 	Events.connect("game_paused", self, "_game_paused")
 	Events.connect("game_unpaused", self, "_game_unpaused")
 
@@ -10,17 +10,17 @@ func _ready():
 func _game_paused() -> void:
 	var tree = get_tree()
 	tree.paused = true
-	_unhide_mouse()
+	_paused_mouse()
 	
 func _game_unpaused() -> void:
 	var tree = get_tree()
 	tree.paused = false
-	_hide_mouse()
+	_unpaused_mouse()
 
 
-func _hide_mouse() -> void:
-	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+func _unpaused_mouse() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
 	
-func _unhide_mouse() -> void:
+func _paused_mouse() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	
