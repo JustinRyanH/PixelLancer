e8160a0b92c4298cff6a8dbd421b811e0309dc4b
Justin Ryan Hurst <jryanhurst@gmail.com>
2021-09-06 16:47:52 -0500

    refactor: Remove rotation debug labels

--- Scripts/DebugUI.gd
+++ Scripts/DebugUI.gd
@@ -3,10 +3,6 @@ extends Control
 onready var fuel_progress_bar: ProgressBar = $Ship/FuelBox/FuelBar
 onready var fuel_recharge: ProgressBar = $Ship/FuelBox/RechargeBar
 onready var speed_value_label: Label = $Ship/SpeedBox/SpeedValue
-onready var target_angle_label: Label = $RotationSection/TargetAngle/Value
-onready var actual_angle_label: Label = $RotationSection/ActualAngle/Value
-onready var angular_velocity_label: Label = $RotationSection/AngularVelocity/Value
-onready var angular_damping_label: Label = $RotationSection/AngularDamping/Value
 onready var debug_checkbox: CheckBox = $RotationSection/DebugMode/CheckBox
 var _main_ship: ThrustedShip = null
 
@@ -28,14 +24,8 @@ func _process(_delta:  float) -> void:
 
 func update_main_ship() -> void:
 	speed_value_label.text = "%2.f" % _main_ship.linear_velocity.length()
-	angular_velocity_label.text = "%2.2f" % _main_ship.angular_velocity
-	angular_damping_label.text = "%2.2f" % _main_ship.angular_damp
-	actual_angle_label.text = "%2.4f" % _main_ship.rotation
 	fuel_progress_bar.value = _main_ship.fuel
 	fuel_recharge.value = _main_ship.fuel_recharge_timer.time_left
-	var rotator: ShipRotatorV2 = _main_ship.get_node("ShipRotator")
-	if rotator:
-		target_angle_label.text = "%2.4f" % rotator.target_angle
 
 func _on_Debug_CheckBox_toggled(button_pressed: bool):
 	Debug.is_debug = button_pressed
